c3160c5147c40f6c9349d9bec2bf12c30e1c04ca
Zana Dauti <zanad_4@hotmail.com>
2015-10-22 16:18:22 +0200

    hej hej

--- app/src/main/java/gruppe180/smarthome/StartActivity.java
+++ app/src/main/java/gruppe180/smarthome/StartActivity.java
@@ -13,6 +13,7 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
