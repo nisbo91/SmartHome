55e52d2fa038f4939f850219e1153028d5da4233
Jesper de Fries <ratonjj@hotmail.com>
2015-10-29 12:39:32 +0100

    småting

--- app/src/main/java/gruppe180/smarthome/StartActivity.java
+++ app/src/main/java/gruppe180/smarthome/StartActivity.java
@@ -1,12 +1,9 @@
 package gruppe180.smarthome;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity{
 
