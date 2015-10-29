44064d2a2e9dae55c4df3ccd777e9fdf5b358e32
Jimmy <nisbo91@gmail.com>
2015-10-29 15:57:13 +0100

    add icon

--- app/src/main/java/gruppe180/smarthome/LoginFragment.java
+++ app/src/main/java/gruppe180/smarthome/LoginFragment.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
 import android.nfc.NfcAdapter;
-import android.nfc.NfcManager;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.provider.Settings;
@@ -18,9 +17,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class FragmentLogin extends Fragment implements View.OnClickListener, Preference.OnPreferenceChangeListener {
+public class LoginFragment extends Fragment implements View.OnClickListener, Preference.OnPreferenceChangeListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -38,10 +36,10 @@ public class FragmentLogin extends Fragment implements View.OnClickListener, Pre
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentLogin.
+     * @return A new instance of fragment LoginFragment.
      */
-    public static FragmentLogin newInstance(String param1, String param2) {
-        FragmentLogin fragment = new FragmentLogin();
+    public static LoginFragment newInstance(String param1, String param2) {
+        LoginFragment fragment = new LoginFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -49,7 +47,7 @@ public class FragmentLogin extends Fragment implements View.OnClickListener, Pre
         return fragment;
     }
 
-    public FragmentLogin() {
+    public LoginFragment() {
         // Required empty public constructor
     }
 
@@ -72,7 +70,7 @@ public class FragmentLogin extends Fragment implements View.OnClickListener, Pre
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        Log.d("FragmentLogin", "Fragment onCreate()");
+        Log.d("LoginFragment", "Fragment onCreate()");
         View login = inflater.inflate(R.layout.fragment_login,container,false);
 
         getActivity().registerReceiver(broadcastReceiver, new IntentFilter(NfcAdapter.ACTION_ADAPTER_STATE_CHANGED));
