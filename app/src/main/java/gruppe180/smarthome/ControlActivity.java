92cadea462027d87b4edaca37e600eec32b195ce
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2015-11-08 15:35:56 +0100

    Sat en viewpager ind, der viser to styk ControlOnOffFragment

--- app/src/main/java/gruppe180/smarthome/ControlActivity.java
+++ app/src/main/java/gruppe180/smarthome/ControlActivity.java
@@ -1,9 +1,17 @@
 package gruppe180.smarthome;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ControlActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,9 +20,12 @@ public class ControlActivity extends AppCompatActivity {
 
         if (savedInstanceState==null) {
             getSupportFragmentManager().beginTransaction().add(R.id.topFrame, new StreamFragment(), "top").commit();
-            getSupportFragmentManager().beginTransaction().add(R.id.bottomFrame, new ControlOnOffFragment(), "bot").commit();
+            //getSupportFragmentManager().beginTransaction().add(R.id.bottomFrame, new ControlOnOffFragment(), "bot").commit();
             //getSupportFragmentManager().beginTransaction().add(R.id.bottomFrame, new ControlDirectionFragment(), "bot").commit();
         }
+
+        viewPager = (ViewPager) findViewById(R.id.bottomFrame);
+        viewPager.setAdapter(new VPAdapter(getSupportFragmentManager()));
     }
 
     public void setNewBottomFragment(){
@@ -23,4 +34,19 @@ public class ControlActivity extends AppCompatActivity {
 
     }
 
+    private class VPAdapter extends FragmentPagerAdapter {
+        public VPAdapter(FragmentManager supportFragmentManager) {
+            super(supportFragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new ControlOnOffFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
 }
