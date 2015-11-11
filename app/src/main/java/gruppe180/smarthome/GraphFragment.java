6a9210ba5542dee13d6827ec020fd95ec3d78b90
Zana Dauti <zanad_4@hotmail.com>
2015-11-11 18:31:01 +0100

    Graph implementeret med resten af projektet

--- app/src/main/java/gruppe180/smarthome/GraphFragment.java
+++ app/src/main/java/gruppe180/smarthome/GraphFragment.java
@@ -9,6 +9,12 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Calendar;
+import java.util.Date;
 
 
 /**
@@ -87,6 +93,37 @@ public class GraphFragment extends Fragment {
         LinearLayout layout = (LinearLayout) view.findViewById(R.id.graph);
         layout.addView(graphView);
         // Inflate the layout for this fragment
+
+        //setContentView(R.layout.fragment_graph);
+       // view.setContentDescription(R.layout.fragment_graph);
+        // generate Dates
+        Calendar calendar = Calendar.getInstance();
+        Date d1 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d2 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d3 = calendar.getTime();
+
+        GraphView graph = (GraphView) view.findViewById(R.id.graph);
+
+// you can directly pass Date objects to DataPoint-Constructor
+// this will convert the Date to double via Date#getTime()
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(d1, 1),
+                new DataPoint(d2, 5),
+                new DataPoint(d3, 3)
+        });
+        graph.addSeries(series);
+        // set date label formatter
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
+        //graph.getGridLabelRenderer().setNumVerticalLabels(8);
+
+        // set manual x bounds to have nice steps
+        graph.getViewport().setMinX(d1.getTime());
+        graph.getViewport().setMaxX(d3.getTime());
+        graph.getViewport().setXAxisBoundsManual(true);
+
         return view;
     }
 
