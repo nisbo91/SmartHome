05f901ea8f3bea01a6a27cf7dd06f0e4526a2c2c
Peter1972 <s102810@student.dtu.dk>
2015-11-18 10:25:43 +0100

    rename singleton

---
+++ app/src/main/java/gruppe180/smarthome/InternalDataSingleton.java
@@ -0,0 +1,37 @@
+package gruppe180.smarthome;
+
+/**
+ * Created by Peter1972 on 12-11-2015.
+ */
+public class InternalDataSingleton {
+    private static InternalDataSingleton mInstance = null;
+
+    // variable declaration
+    private String mString;
+
+    // variable value settings
+    private InternalDataSingleton(){
+        mString = "Hello";
+    }
+
+    // If no singleton then start new singleton
+    public static InternalDataSingleton getInstance(){
+        if(mInstance == null)
+        {
+            mInstance = new InternalDataSingleton();
+        }
+        return mInstance;
+    }
+
+    // Singleton methods
+
+    public String getString(){
+        return this.mString;
+    }
+
+    public void setString(String value){
+        mString = value;
+        System.out.println("hej");
+    }
+
+}
