1d7646cbc2040d6e6c930c9fe352cebb97d49a72
Peter1972 <s102810@student.dtu.dk>
2015-11-25 16:04:04 +0100

    InternalDataSingleton

--- app/src/main/java/gruppe180/smarthome/InternalDataSingleton.java
+++ app/src/main/java/gruppe180/smarthome/InternalDataSingleton.java
@@ -1,18 +1,16 @@
 package gruppe180.smarthome;
 
-/**
- * Created by Peter1972 on 12-11-2015.
- */
+import java.util.ArrayList;
+
 public class InternalDataSingleton {
     private static InternalDataSingleton mInstance = null;
 
     // variable declaration
-    private String mString;
+    private ArrayList<String> user_data;
+    private ArrayList<String> card_data;
+    private String[] cards;
+    private String[][] users;
 
-    // variable value settings
-    private InternalDataSingleton(){
-        mString = "Hello";
-    }
 
     // If no singleton then start new singleton
     public static InternalDataSingleton getInstance(){
@@ -25,13 +23,55 @@ public class InternalDataSingleton {
 
     // Singleton methods
 
-    public String getString(){
-        return this.mString;
+    public void setUserdata(String cardid, String user, String pass, String email, String ip ){
+
+        user_data = new ArrayList<>();
+        card_data = new ArrayList<>();
+
+        card_data.add(cardid);
+        user_data.add(user);
+        user_data.add(pass);
+        user_data.add(email);
+        user_data.add(ip);
     }
 
-    public void setString(String value){
-        mString = value;
-        System.out.println("hej");
+    private void convert_cards() {
+
+        int size = card_data.size();
+        cards = new String[size];
+
+        for (int i = 0; i < size; i++) {
+            String y = card_data.get(i);
+            cards[i] = y;
+        }
+    }
+
+    private void convert_users() {
+
+        int size = user_data.size();
+        System.out.println("size = "+size);
+        int rows = size/4;
+        System.out.println("rows = "+rows);
+        int col = 4;
+        users = new String[rows][col];
+
+        for(int y = 0; y < rows; y++){
+            users[y] = new String[col];
+            for(int x = 0; x < col; x++){
+                users[y][x] = user_data.get(y*col + x);
+            }
+        }
+
+    }
+
+    public String[] getCarddata(){
+        convert_cards();
+        return cards;
+    }
+
+    public String[][] getUserdata(){
+        convert_users();
+        return users;
     }
 
 }
